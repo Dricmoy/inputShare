ae8e50b2a7da0b5ecfacc1b927a5b19c9d382cc1
Dricmoy Bhattacharjee <dricbhattacharjee@gmail.com>
2025-07-22 11:17:23 -0600

    feature: reciever.py will handle logic for mouse position recieving from host pc

---
+++ reciever.py
@@ -0,0 +1,24 @@
+import socket
+import json
+from pyinput.mouse import Controller
+
+mouse = Controller()
+
+HOST = '0.0.0.0'
+PORT = 5050
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen(1)
+
+print("[*] Waiting for connection")
+conn, addr = server.accept()
+print(f"[+] Connected by {addr}")
+
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+
+    coords = json.loads(data.decode('utf-8'))
+    mouse.position = (coords['x'] - 1000, coords['y'])
